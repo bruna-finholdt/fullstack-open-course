89b4b9ec2a2c63c154d6fe323733677376e0434a
Bruna Finholdt <brunaperez1991@gmail.com>
2023-05-24 17:45:29 -0300

    part 1 unicafe step 2 - exercise 1.7

--- part-1/unicafe/src/App.js
+++ part-1/unicafe/src/App.js
@@ -2,22 +2,43 @@ import { useState } from "react";
 import Button from "./components/Button";
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [clicks, setClicks] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  })
+  const [all, setAll] = useState(0);
 
   const handleGoodClick = () => {
-    setGood(good + 1)
-  }
+    const newClicks = {
+      ...clicks,
+      good: clicks.good + 1,
+    }
+    setClicks(newClicks)
+    setAll(newClicks.good + clicks.bad + clicks.neutral)
+  };
 
-    const handleNeutralClick = () => {
-      setNeutral(neutral + 1);
+  const handleNeutralClick = () => {
+    const newClicks = {
+      ...clicks,
+      neutral: clicks.neutral + 1,
     };
+    setClicks(newClicks);
+    setAll(newClicks.neutral + clicks.bad + clicks.good);
+  };
 
-      const handleBadClick = () => {
-        setBad(bad + 1);
-      };
+  const handleBadClick = () => {
+const newClicks = {
+  ...clicks,
+  bad: clicks.bad + 1,
+};
+setClicks(newClicks);
+setAll(newClicks.bad + clicks.good + clicks.neutral);
+  };
+
+  const average = (clicks.good + clicks.neutral + clicks.bad) / 3;
+
+  const positive = clicks.good ? `${(clicks.good / all) * 100}  %` : "0 %";
 
   return (
     <div>
@@ -26,9 +47,12 @@ const App = () => {
       <Button text={"neutral"} handleClick={handleNeutralClick} />
       <Button text={"bad"} handleClick={handleBadClick} />
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <p>good {clicks.good}</p>
+      <p>neutral {clicks.neutral}</p>
+      <p>bad {clicks.bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
     </div>
   );
 };
