bc1049fda02187c56553fee616c0b194779d883a
Bruna Finholdt <brunaperez1991@gmail.com>
2023-05-25 15:04:19 -0300

    part 1 unicafe step 4 - exercise 1.9

--- part-1/unicafe/src/components/Statistics.js
+++ part-1/unicafe/src/components/Statistics.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const Statistics = ({good, neutral, bad, all, average, positive}) => {
+const Statistics = ({ good, neutral, bad, all, average, positive }) => {
   return (
     <div>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive}</p>
+
+      {all > 0 ? (
+        <>
+          <p>good {good}</p>
+          <p>neutral {neutral}</p>
+          <p>bad {bad}</p>
+          <p>all {all}</p>
+          <p>average {average}</p>
+          <p>positive {positive}</p>
+        </>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
   );
-}
+};
 
-export default Statistics
+export default Statistics;
