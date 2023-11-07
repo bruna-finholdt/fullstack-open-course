b83f7f834da83fb50515b2757217f5fdeda7b411
Bruna Finholdt Lamboglia Perez <brunaperez1991@gmail.com>
2023-11-07 09:29:34 -0300

    part 3 phonebook database steps 1 and 2 exercises 3.13 and 3.14

--- part-3/phonebook/README.md
+++ part-3/phonebook/README.md
@@ -2,7 +2,7 @@
 
 ## Overview
 
-This is a simple Phonebook application that allows you to manage your contacts. You can add, edit (not working yet), and delete contacts using this application.
+This is a simple Phonebook application that allows you to manage your contacts. You can add, edit, and delete contacts using this application.
 
 ## Technologies Used
 
@@ -13,12 +13,12 @@ This is a simple Phonebook application that allows you to manage your contacts. 
 
 - View a list of contacts
 - Add a new contact
-- Delete a contact
 - Filter and search for contacts
 
 ## Roadmap features
 
-- Update contact information (not working yet)
+- Update contact information
+- Delete a contact
 
 ## Setup
 
@@ -28,25 +28,19 @@ To run this application locally, follow these steps:
 
    [git clone](https://github.com/bruna-finholdt/fullstack-open-course)
 
-2. Change to the frontend project directory and install dependencies:
+2. Change to the frontend project directory, install dependencies and start the frontend:
 
    cd part-2/phonebook
 
    npm install
-
-   Start the frontend:
 
    npm start
 
-3. Change to the backend project directory and install dependencies:
-
-   cd part-2/phonebook
-
-   npm install
-
-   Start the backend:
+3. Change to the backend project directory, install dependencies and start the backend:
 
    cd part-3/phonebook
+
+   npm install
 
    npm run dev
 
@@ -59,7 +53,6 @@ To run this application locally, follow these steps:
 - `GET /api/persons` - Get a list of all contacts
 - `GET /api/persons/:id` - Get a contact by ID
 - `POST /api/persons` - Create a new contact
-- `DELETE /api/persons/:id` - Delete a contact by ID
 
 ## Deployment
 
--- part-3/phonebook/index.js
+++ part-3/phonebook/index.js
@@ -1,110 +1,59 @@
+require('dotenv').config();
+const mongoose = require('mongoose')
 const express = require('express')
 var morgan = require('morgan')
-const app = express()
 const cors = require('cors')
+const Person = require('./models/person')
+
+const app = express()
+ 
 app.use(express.static('build'))
-
 app.use(cors())
-
 morgan.token('req-body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] - :req-body'));
-
-// app.use(morgan('tiny'));
-
 app.use(express.json())
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-  ]
-  
-  app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-  })
+ 
  
   app.get('/api/persons', (request, response) => {
-    response.json(persons)
-  })
-
+    Person.find({}).then((persons) => {
+      response.json(persons);
+    });
+  });
+ 
+ 
   app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    // const { id } = req.params
-    const person = persons.find(person => person.id === id)
-    if (person) {
+    Person.findById(request.params.id).then(person => {
+      if (person) {
         response.json(person)
       } else {
         response.status(404).end()
       }
-  })
-
-  app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
-  })
-
-  const generateId = () => {
-    const maxId = persons.length > 0
-      ? Math.max(...persons.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
-  
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
+  })  
+ 
+   
   app.post('/api/persons', (request, response) => {
     const body = request.body
-  
+ 
     if (!body.name || !body.number) {
-      return response.status(400).json({ 
-        error: 'name and number required' 
-      })
+      return response.status(400).json({ error: 'name and number required' })
     }
-
-    const nameExists = persons.find(person => person.name === body.name);
-
-    if (nameExists) {
-      return response.status(400).json({ 
-        error: 'name must be unique' 
-      })
-    }
-  
-    const person = {
+ 
+    const person = new Person({
       name: body.name,
       number: body.number,
-      id: generateId(),
-    }
-  
-    persons = persons.concat(person)
-  
-    response.json(person)
+    })  
+ 
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
   })
-
-  app.get('/info', (request, response) => {
-    const info = `<p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>
-    `
-    response.send(info)
-  })
-  
-  const PORT = process.env.PORT || 3001
+ 
+  const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
