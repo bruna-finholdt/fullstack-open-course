c4e4eb70d347e4b37870e26de6a5efe5c62598ae
Bruna Finholdt Lamboglia Perez <brunaperez1991@gmail.com>
2023-11-07 16:36:37 -0300

    part 3 phonebook database, step 8 - exercise 3.20

--- part-3/phonebook/models/person.js
+++ part-3/phonebook/models/person.js
@@ -20,7 +20,20 @@ mongoose.connect(url)
     },
     number: {
       type: String,
-      required: true
+      // minlength: 8,
+      required: true,
+      validate: {
+        validator: function(value) {
+          const parts = value.split('-');
+          if (parts.length === 2) {
+            const firstPart = parts[0];
+            const secondPart = parts[1];
+            return firstPart.length >= 2 && firstPart.length <= 3 && (firstPart.length + secondPart.length) >= 8;
+          }
+          return false;
+        },
+        message: 'Phone number must have the format xx-xxxxxx or xxx-xxxxx'
+      }
     },
   });
  
