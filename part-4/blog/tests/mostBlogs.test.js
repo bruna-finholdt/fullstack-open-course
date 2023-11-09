e25419da02284ef7f9862a8fce391b3a1afd9beb
Bruna Finholdt Lamboglia Perez <brunaperez1991@gmail.com>
2023-11-09 08:26:21 -0300

    part 4 helper functions and unit tests step 4 exercise 4.6

---
+++ part-4/blog/tests/mostBlogs.test.js
@@ -0,0 +1,128 @@
+const listHelper = require('../utils/list_helper')
+
+describe('author with more blogPosts', () => {
+
+  const blogsList = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: '5a422b891b54a676234d17fa',
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: '5a422ba71b54a676234d17fb',
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422bc61b54a676234d17fc',
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      likes: 2,
+      __v: 0
+    }
+  ]
+
+  const blogsList2TopAuthors = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: '5a422b891b54a676234d17fa',
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: '5a422ba71b54a676234d17fb',
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422bc61b54a676234d17fc',
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      likes: 2,
+      __v: 0
+    }
+  ]
+
+  test('the author with most posts', () => {
+    const result = listHelper.mostBlogs(blogsList)
+    expect(result).toEqual({ author: 'Robert C. Martin', blogs: 3 })
+  })
+
+  test('the author with most posts', () => {
+    const result = listHelper.mostBlogs(blogsList2TopAuthors)
+    expect(result).toEqual({ author: 'Edsger W. Dijkstra', blogs: 3 })
+    //Edsger and Robert have the same amount of posts, so both are the authors with most posts, but since I expect only one name as a return and lodash returns the one that comes first in the list, the correct expect is Edsger, that comes before Robert.
+  })
+
+})
+
+//In lodash, when you use the maxBy function to find the maximum value based on a specific property, it will return the first element that has the maximum value for that property. This means that if you have two authors with the same number of blog posts, maxBy will return the first one it encounters in the array. Both 'Edsger W. Dijkstra' and 'Robert C. Martin' have the same number of blog posts (3). In this case, maxBy will return the first author encountered, which is 'Edsger W. Dijkstra'.
--- part-4/blog/utils/list_helper.js
+++ part-4/blog/utils/list_helper.js
@@ -1,3 +1,5 @@
+const { groupBy, maxBy } = require('lodash')
+
 const dummy = (blogPosts) => {
   const result = blogPosts.length - blogPosts.length + 1
   return result
@@ -16,12 +18,24 @@ const totalLikes = (blogPosts) => {
 const favoriteBlog = (blogPosts) => {
   const favoriteBlogPost = blogPosts.reduce((previousBlogPost, currentBlogPost) => {
     return previousBlogPost.likes > currentBlogPost.likes ? previousBlogPost : currentBlogPost
-  })
+  }, {})
   return favoriteBlogPost
+}
+
+const mostBlogs = (blogPosts) => {
+  const blogsByAuthor = groupBy(blogPosts, 'author')
+  console.log('blogsByAuthor', blogsByAuthor)
+  //A função maxBy espera um array de objetos e um campo pelo qual deve encontrar o valor máximo. No entanto, você está passando blogsByAuthor, que é um objeto, não um array, para maxBy. É por isso que você está recebendo undefined. Para encontrar o autor com mais posts, você deve transformar o objeto blogsByAuthor em um array de objetos (um objeto para cada autor) antes de usar maxBy. Você pode fazer isso usando Object.entries para obter as entradas do objeto e, em seguida, mapear essas entradas para um formato que maxBy possa usar.
+  const authorPostsArray = Object.entries(blogsByAuthor).map(([author, posts]) => ({ author, blogs: posts.length }))
+  console.log('authorPostsArray', authorPostsArray)
+  const authorWithMostPosts = maxBy(authorPostsArray, 'blogs')
+  console.log('authorWithMostPosts', authorWithMostPosts)
+  return authorWithMostPosts ? authorWithMostPosts : null
 }
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
