cc1591781b9422f5c6e160dac736c28c7c4fb109
Bruna Finholdt Lamboglia Perez <brunaperez1991@gmail.com>
2024-01-04 11:20:16 -0300

    part 5 bloglist tests, step4, exercise 5.16

--- part-5/bloglist-frontend/src/components/BlogPostForm.test.js
+++ part-5/bloglist-frontend/src/components/BlogPostForm.test.js
@@ -4,18 +4,24 @@ import '@testing-library/jest-dom'
 import BlogPostForm from './BlogPostForm'
 import userEvent from '@testing-library/user-event'
 
-test('<BlogPostForm /> updates parent state and calls onSubmit', async () => {
+test('the form calls the event handler it received as props with the right details when a new blog is created', async () => {
   const handleNewBlogPost = jest.fn()
   const user = userEvent.setup()
 
   render(<BlogPostForm handleNewBlogPost={handleNewBlogPost} />)
 
   const input = screen.getByPlaceholderText('title')
+  const input2 = screen.getByPlaceholderText('author')
+  const input3 = screen.getByPlaceholderText('url')
   const sendButton = screen.getByText('create')
 
-  await user.type(input, 'testing a form...')
+  await user.type(input, 'Title')
+  await user.type(input2, 'Author')
+  await user.type(input3, 'www.test.com')
   await user.click(sendButton)
 
   expect(handleNewBlogPost.mock.calls).toHaveLength(1)
-  expect(handleNewBlogPost.mock.calls[0][0].title).toBe('testing a form...')
+  expect(handleNewBlogPost.mock.calls[0][0].title).toBe('Title')
+  expect(handleNewBlogPost.mock.calls[0][0].author).toBe('Author')
+  expect(handleNewBlogPost.mock.calls[0][0].url).toBe('www.test.com')
 })
